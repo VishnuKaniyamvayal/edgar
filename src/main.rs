6825c067eab4a729c7231eebd7922fb666438f7c
vishnu vinod <vishnuvinodkvl@gmail.com>
2026-04-28 21:26:43 +0530

    minor bugs fixed

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ async fn main() {
 
     let app = routers::init_router(app_state);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0::3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     axum::serve(listener, app).await.unwrap()
 }
